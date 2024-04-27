2db9ad7e4a4b0017a8c38e9e5ecfddface6888d6
mendarrr <sarahabby541@gmail.com>
2024-04-27 22:16:55 +0300

    Initial commit

---
+++ bank-of-flatiron/gh-pages.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Path to your index.html file
+    src: 'build/index.html',
+    // Path to your GitHub Pages site
+    repo: 'https://mendarrr.github.io/ABBY-Phase-2-Code_Challenge/',
+    // Branch to deploy to
+    branch: 'gh-pages'
+  };
---
+++ bank-of-flatiron/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Transaction from './components/Transaction';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Transaction />
+    </div>
+  );
+}
+
+export default App;
